50201e5d9d9072b95a2fbd98cfd47d715375a4b7
Pinto-PINTO <marie334fe@gmail.com>
2024-03-29 13:23:43 -0400

    Pushed Regions and IOS details

--- src/views/dashboard/components/RecentTransactions.js
+++ src/views/dashboard/components/RecentTransactions.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import DashboardCard from '../../../components/shared/DashboardCard';
 import {
   Timeline,
@@ -8,67 +8,71 @@ import {
   TimelineDot,
   TimelineConnector,
   TimelineContent,
-  timelineOppositeContentClasses,
 } from '@mui/lab';
 import { Link, Typography } from '@mui/material';
 
 const RecentTransactions = () => {
+  const [countryData, setCountryData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const responsePromises = [
+          fetch('https://api.worldbank.org/v2/country/Z7?format=json'),
+          fetch('https://api.worldbank.org/v2/country/ZQ?format=json'),
+          fetch('https://api.worldbank.org/v2/country/Z4?format=json'),
+          fetch('https://api.worldbank.org/v2/country/ZG?format=json'),
+          fetch('https://api.worldbank.org/v2/country/8S?format=json'),
+        ];
+
+        const responses = await Promise.all(responsePromises);
+        const dataPromises = responses.map((response) => response.json());
+        const countryData = await Promise.all(dataPromises);
+
+        setCountryData(countryData);
+      } catch (error) {
+        console.error('Error fetching country data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <DashboardCard title="USA Cities and Postal Codes">
-      <>
-        <Timeline
-          className="theme-timeline"
-          nonce={undefined}
-          onResize={undefined}
-          onResizeCapture={undefined}
-          sx={{
-            p: 0,
-            mb: '-40px',
-            '& .MuiTimelineConnector-root': {
-              width: '1px',
-              backgroundColor: '#efefef'
-            },
-            [`& .${timelineOppositeContentClasses.root}`]: {
-              flex: 0.5,
-              paddingLeft: 0,
-            },
-          }}
-        >
-          <TimelineItem>
-            <TimelineOppositeContent>ISO 2 Code</TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineDot color="secondary" variant="outlined" />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>
-              <Typography fontWeight="600">Country</Typography>{' '}
-              <Link href="/" underline="none">
-                Capital City
-              </Link>
-            </TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineOppositeContent>10:00 am</TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineDot color="secondary" variant="outlined" />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>
-              <Typography fontWeight="600">New sale recorded</Typography>{' '}
-              <Link href="/" underline="none">
-                #ML-3467
-              </Link>
-            </TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineOppositeContent>12:00 am</TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineDot color="success" variant="outlined" />
-            </TimelineSeparator>
-            <TimelineContent>Payment Received</TimelineContent>
-          </TimelineItem>
-        </Timeline>
-      </>
+    <DashboardCard title="Regions and ISO Codes">
+      <Timeline
+        className="theme-timeline"
+        sx={{
+          p: 0,
+          mb: '-40px',
+          '& .MuiTimelineConnector-root': {
+            width: '1px',
+            backgroundColor: '#efefef'
+          },
+          textAlign: 'left' // Added to align content to the left
+        }}>
+        {countryData.map((country, index) => {
+          const iso2Code = country[1]?.[0]?.iso2Code;
+          const name = country[1]?.[0]?.name;
+          const capitalCity = country[1]?.[0]?.capitalCity;
+
+          return (
+            <TimelineItem key={index}>
+              <TimelineOppositeContent>{iso2Code}</TimelineOppositeContent>
+              <TimelineSeparator>
+                <TimelineDot color="secondary" variant="outlined" />
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Typography fontWeight="600">{name}</Typography>{' '}
+                <Link href="/" underline="none">
+                  {capitalCity}
+                </Link>
+              </TimelineContent>
+            </TimelineItem>
+          );
+        })}
+      </Timeline>
     </DashboardCard>
   );
 };
